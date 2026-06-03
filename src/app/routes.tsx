ca99f741ce846312b88fb687077d3b939403e30b
kkg-kevin <kiharakevin505@gmail.com>
2026-06-03 12:16:32 +0300

    improved UI for major sections

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/Layout";
 import { DashboardPage } from "./pages/DashboardPage";
+import { DeploySupplementaryCoursePage } from "./pages/DeploySupplementaryCoursePage";
 import { CurriculumListPage } from "./pages/CurriculumListPage";
 import { CreateCurriculumWizard } from "./pages/CreateCurriculumWizard";
 import { CurriculumDetailPage } from "./pages/CurriculumDetailPage";
@@ -9,6 +10,7 @@ import { AssignmentsPage } from "./pages/AssignmentsPage";
 import { CurriculumSettingsPage } from "./pages/CurriculumSettingsPage";
 import { SchoolDetailPage } from "./pages/SchoolDetailPage";
 import { SchoolsPage } from "./pages/SchoolsPage";
+import { VersionControlPage } from "./pages/VersionControlPage";
 
 export const router = createBrowserRouter([
   {
@@ -25,7 +27,9 @@ export const router = createBrowserRouter([
       { path: "schools/:id", Component: SchoolDetailPage },
       { path: "assignments", Component: AssignmentsPage },
       { path: "assignments/create", Component: AssignCurriculumPage },
+      { path: "deployments/supplementary-course", Component: DeploySupplementaryCoursePage },
       { path: "settings", Component: CurriculumSettingsPage },
+      { path: "settings/version-control", Component: VersionControlPage },
     ],
   },
 ]);
